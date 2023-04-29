d2c317e0cbc1224ab70b525d5563a2677ba68863
Tabassum <127255282+Tsoyeb@users.noreply.github.com>
2023-04-29 11:05:34 +0100

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,12 @@
+# A program to gather information from user.
+name = input("Please enter your name: ")
+print('Hello '+ name)
+age = int(input("Please enter your age: "))
+print(age)
+print('Hello World!')
+
+# Made a sentence with the above information to complete the welcome. 
+print('Hello '+ name + ' you are '+ str(age) + ' welcome to the world!')
+
+
+
---
+++ menu.py
@@ -0,0 +1,8 @@
+# A Program for the user to Order food from a selected menu. 
+print('Hello welcome to my restaurant! Please see the below menu')
+Menu =('pizza, burgers, sausage, chips, pie, fishfingers ')
+print(Menu)
+item1 = input ('Please enter your first favouriate item off the menu or done if finished')
+item2 =input ('please enter your second favouriate item off the menu ')
+item3 =input ('Please enter your third favouriate item off the menu ')
+print('Order confirmation: ' + item1 + ', ' + item2 +', '+  item3 )
